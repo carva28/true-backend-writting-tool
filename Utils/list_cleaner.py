13680f9ba29786e056a3cdade50a460967f64a06
Martinho Mota <m.vaz.mota@gmail.com>
2022-09-08 11:17:11 +0100

    Utils limpeza de listas e remover plural

--- Utils/list_cleaner.py
+++ Utils/list_cleaner.py
@@ -1,13 +1,5 @@
 import difflib as dl
 
-str1 = "Testes"
-str2 = "testes"
-str3 = "teste"
-sug = ["exame","teste","test","teste","testes","téste"]
-
-print("init: ")
-print(sug)
-
 # remove duplicates in list while maintaining order
 def remove_duplicates(iList):
     output = list(dict.fromkeys(iList))
@@ -31,18 +23,3 @@ def to_lowercase(item):
     output = item.lower()
     print(output)
     return output
-
-def remove_plural_words(iList, item = ""):
-    output = [x for x in iList if check_plural_s(x, item)]
-    
-    print(output)
-    return output
-    
-# Os substantivos e adjectivos que no singular terminam em a, e, i, o ou u formam o plural com o acrescentamento de um s     
-def check_plural_s(item):
-    output = item
-    print(output)
-    return output
-    
-remove_plural_words(sug)
-

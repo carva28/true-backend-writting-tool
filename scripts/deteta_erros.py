e00b4102f749cd79613f768dcc7b6ff0286dcc3e
Martinho Mota <m.vaz.mota@gmail.com>
2022-11-08 14:50:59 +0000

    update detata erros

--- scripts/deteta_erros.py
+++ scripts/deteta_erros.py
@@ -18,21 +18,44 @@ class CorrectionResult:
         "startRange": self.startRange,
         "endRange": self.endRange}
 
-def check_last_char(word):
-    valid_chars = ["?", "!", ",", ";", "'", '"', ":"]
+def check_special_char(word):
+    new_word = word
+    valid_second_last_chars = [ ";", ".", "]", "'", '"', ")", "?", "!" ]
+    valid_last_chars = [ ",", ";", ".", "]", "'", '"', ")", ":","?", "!" ]
+    valid_first_chars = [ "'", '"', "(" ,"["]
+    valid_second_chars = [ "'", '"', "(" ,"["]
 
-    # check if is valid special char
+    # check if last char is valid stop char
+    if any(new_word[-1] in char for char in valid_last_chars):
+        new_word = new_word[:-1]
+
+        # check if second last char is also valid
+        if any(new_word[-1] in char for char in valid_second_last_chars):
+            # check if last and second last chars are different
+            if(word[-1] != word[-2]):
+                new_word = new_word[:-1]
+
+    # check if first char is valid
+    if any(new_word[0] in char for char in valid_first_chars):
+        new_word = new_word[1:]
+
+        # check if second char is also valid
+        if any(new_word[0] in char for char in valid_second_chars):
+            # check if first and second chars are different
+            if(word[0] != word[1]):
+                new_word = new_word[1:]
+
+    return new_word
+
+
+def check_valid_number(word):
+    valid_chars = ["%", "$", "£", "€", "º", "ª", ",", ".", "!", "?", ":"]
+
+    # last char is valid
     if any(word[-1] in char for char in valid_chars):
-        if word[0] == "'" or word[0] == '"':
-            return word[1:-1]
-        elif word[-1] == "." and word[-2] == '"':
-            return word[:-2]
-        elif word[-1] == "." and word[-2] == "'":
-            return word[:-2]     
-        else:
-            return word[:-1]
-    else:
-        return word
+        word = word[:-1]
+
+    return word    
 
 def check_text(text):
     words = [CorrectionResult()]
@@ -58,12 +81,24 @@ def check_text(text):
         del words[-1]
 
     for word in words:
-        # if last char is valid special char        
-        word.word = check_last_char(word.word)
+        print("initial word ", word.word)
+        # check if is number with special char
+        if(word.word[0].isnumeric()):
+            word.word = check_valid_number(word.word)
+        else:
+            # check if last char is valid        
+            word.word = check_special_char(word.word)
 
         word.correct = chkr.check(word.word)
         sugestion = chkr.suggest(word.word)
         word.suggestion = sugestion[0] if sugestion else ""
+
+        print(word.word, " ", word.correct)
+        print("-----")
+
    
     results = [obj.to_dict() for obj in words]
+
     return results
+
+check_text("2 20 20,12 20.11 20% 20,2%")
